b69e807d84cedec260d4f1653896f541b0ade51c
dalma <dalmamf@gmail.com>
2020-07-13 15:19:06 -0400

    creating dac pac

---
+++ tamo/tamo.database/tables/Continent.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[Continent]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(MAX) NOT NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Primary Key',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Continent',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Name of the Continent',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Continent',
+    @level2type = N'COLUMN',
+    @level2name = N'Name'
